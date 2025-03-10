ab241fb797b18267ccc521dc57f664d265c18281
erick <erickestradarosario@gmail.com>
2025-03-10 19:05:53 -0400

    Crea Modelo Encuestas

---
+++ Erick_Estrada_P2_AP1/Models/Encuestas.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Erick_Estrada_P2_AP1.Models;
+
+public class Encuestas
+{
+    [Key]
+    public int EncuestaId { get; set; }
+    
+    public DateTime FechaEncuesta { get; set; }
+    
+    [Required]
+    [MaxLength(50)]
+    public string Asignatura { get; set; }
+    
+    public List<EncuestasDetalle> EncuestasDetalles { get; set; } = new List<EncuestasDetalle>();
+}
