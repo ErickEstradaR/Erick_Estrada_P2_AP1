f5fa1f5392fd533aa07edd35707f0b16a50e85a0
erick <erickestradarosario@gmail.com>
2025-03-10 20:12:31 -0400

    Organicé el código

--- Erick_Estrada_P2_AP1/Models/Encuestas.cs
+++ Erick_Estrada_P2_AP1/Models/Encuestas.cs
@@ -1,17 +1,13 @@
 using System.ComponentModel.DataAnnotations;
-
 namespace Erick_Estrada_P2_AP1.Models;
 
 public class Encuestas
 {
     [Key]
     public int EncuestaId { get; set; }
-    
     public DateTime FechaEncuesta { get; set; }
-    
     [Required]
     [MaxLength(50)]
     public string Asignatura { get; set; }
-    
     public List<EncuestasDetalle> EncuestasDetalles { get; set; } = new List<EncuestasDetalle>();
 }
